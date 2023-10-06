23a22b7ec6902d3acf3acff3f1ae492d7b3e95ef
brycoti <bryancozartitos@gmail.com>
2023-10-06 16:42:18 +0200

    register validado por postman

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,11 @@ Route::get('/prueba', function () {
     return "prueba";
 });
 
+Route::post('register', [RegisterController::class, 'store'])->name('api.v1.register');
+
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+*/
