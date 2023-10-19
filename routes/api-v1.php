2d8e1ec4ce8328045fc93baf1d9fe1ff61ec6321
brycoti <bryancozartitos@gmail.com>
2023-10-19 18:33:18 +0200

    Spatie instalado

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -10,22 +10,6 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Passport\Passport;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::get('/prueba', function () {
-    return "prueba";
-});
-
-
 
 Route::post('/players', [UserController::class, 'store'])->name('api.v1.register');
 
@@ -35,17 +19,4 @@ Route::middleware('auth:api')->group(function () {
     Route::put('/players/{id}', [UserController::class, 'update'])->name('api.v1.updateName');
     Route::post('/players/{id}/games', [RollDiceController::class, 'rollIt'])->name('api.v1.rollIt');
     Route::delete('/players/{id}/games', [RollDiceController::class, 'deleteRolls'])->name('api.v1.delete');
-
-    // Otras rutas que requieran el middleware 'auth:api' pueden ir aquí.
 });
-
-
-// Route::middleware('auth:api')->post('roll-dice', 'DiceRollController@rollDice');
-
-
-/*
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-*/
